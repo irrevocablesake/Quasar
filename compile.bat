6659854ad35644645e648032dad58882d8f6e013
sake :) <irrevocablesake@gmail.com>
2025-09-13 12:14:47 +0530

    PACKAGE: Assimp

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 @REM g++ -std=c++20 main.cpp -o program
-g++ -std=c++20 -O3 -march=native -DNDDEBUG main.cpp -o program
+g++ -std=c++20 -O3 -march=native -DNDDEBUG main.cpp -Iassimp -Lassimp/library -lassimp -lz -o program
