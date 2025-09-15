f807f758148a13446ceda5ab7b44d7652ad830cf
sake :) <irrevocablesake@gmail.com>
2025-09-15 23:42:40 +0530

    REFRACTOR: RotationX

--- Transformations.h
+++ Transformations.h
@@ -121,4 +121,85 @@ class RotateY : public Mesh {
         AABB boundingBox;
 };
 
+class RotateX : public Mesh {
+    public:
+        RotateX( shared_ptr< Mesh > mesh, double angle ) : mesh( mesh ){
+            auto radians = degreesToRadians( angle );
+            
+            sinTheta = std::sin( radians );
+            cosTheta = std::cos( radians );
+
+            boundingBox = mesh -> getBoundingBox();
+
+            Point3 min( INF, INF, INF );
+            Point3 max( -INF, -INF, -INF );
+
+            for( int i = 0; i < 2; i++ ){
+                for( int j = 0; j < 2; j++ ){
+                    for( int k = 0; k < 2; k++ ){
+                        auto x = i * boundingBox.x.max + ( 1 - i ) * boundingBox.x.min;
+                        auto y = j * boundingBox.y.max + ( 1 - j ) * boundingBox.y.min;
+                        auto z = k * boundingBox.z.max + ( 1 - k ) * boundingBox.z.min;
+
+                        auto newX = cosTheta * y + sinTheta * z;
+                        auto newZ = -sinTheta * y + cosTheta * z;
+
+                        Vector3 coordinates( x, newX, newZ );
+
+                        for( int c = 0; c < 3; c++ ){
+                            min[c] = std::fmin( min[c], coordinates[c] );
+                            max[c] = std::fmax( max[c], coordinates[c] );
+                        }
+                    }
+                }
+            }
+
+            boundingBox = AABB( min, max );
+        }
+
+        bool hit( const Ray &ray, Interval interval, IntersectionManager &intersectionManager ) const override {
+            auto origin = Point3( 
+                ray.origin().x(),
+                ( cosTheta * ray.origin().y() ) - ( sinTheta * ray.origin().z() ),
+                ( sinTheta * ray.origin().y() ) + ( cosTheta * ray.origin().z() )
+             );
+
+             auto direction = Vector3(
+                ray.direction().x(),
+                ( cosTheta * ray.direction().y() ) - ( sinTheta * ray.direction().z() ),
+                ( sinTheta * ray.direction().y() ) + ( cosTheta * ray.direction().z() ) 
+             );
+
+             Ray objectSpaceRay( origin, direction, ray.time() );
+
+             if( !mesh -> hit( objectSpaceRay, interval, intersectionManager ) ){
+                return false;
+             }
+
+            intersectionManager.point = Point3(
+                intersectionManager.point.x(),
+                ( cosTheta * intersectionManager.point.y() ) + ( sinTheta * intersectionManager.point.z() ),
+                ( -sinTheta * intersectionManager.point.y() ) + ( cosTheta * intersectionManager.point.z() )
+            );
+
+            intersectionManager.normal = Vector3(
+                intersectionManager.normal.x(),
+                ( cosTheta * intersectionManager.normal.y() ) + ( sinTheta * intersectionManager.normal.z() ),
+                ( -sinTheta * intersectionManager.normal.y() ) + ( cosTheta * intersectionManager.normal.z() )
+            );
+
+            return true;
+        }
+
+        AABB getBoundingBox() const override {
+            return boundingBox;
+        }
+
+    private:
+        shared_ptr< Mesh > mesh;
+        double sinTheta;
+        double cosTheta;
+        AABB boundingBox;
+};
+
 #endif
